8b274c32f0c34eef617b18b0f792a6ee8e51a88a
hugobowne <hugobowne@gmail.com>
2015-09-03 15:10:24 -0400

    adding scripts to measure the difference between empirical & modeled probability distributions

---
+++ model/scripts/model_module.R
@@ -0,0 +1,143 @@
+M1 <- function( data , N , xlim = c(0,0.5) , logy =TRUE){
+  #data <- knPairs
+  #set.seed(71)
+  
+  df <- data
+  dat <- as.matrix( df$k2/df$n )
+  
+  N <- sum( df$k2)/sum( df$n)
+  #f <- par$Var2
+  model <- simulate_binomial( N , 0 ,data)
+  
+  model <- as.data.frame(model)
+  model$type <- 'model'
+  colnames(model)[1] <- 'pmin'
+  colnames(dat)[1] <- 'pmin'
+  
+  dat <- as.data.frame(dat)
+  dat$type <- 'data'
+  data <- dat
+  
+  #and combine into your new data frame vegLengths
+  pmin1 <- rbind(data , model )
+  
+  #now make your lovely plot
+  if (logy == TRUE){
+    ggplot(pmin1, aes(pmin, colour = type)) + geom_density(alpha = 0.2 ) + coord_cartesian(xlim=xlim) +
+      scale_y_log10() + annotation_logticks() + theme(legend.position= "none",
+                                                      #axis.text.y=element_blank() , axis.text.x=element_blank(),
+                                                      axis.line=element_blank() , axis.title.x=element_blank() , 
+                                                      axis.title.y=element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=1))} else
+                                                      {ggplot(pmin1, aes(pmin, fill = type)) + geom_density(alpha = 0.2 ) + coord_cartesian(xlim=xlim)}
+  return( pmin1)
+  
+  
+}
+
+M2 <- function( data ,  par , xlim = c(0,0.5) , logy =TRUE){
+  #data <- knPairs
+  #set.seed(71)
+  
+  df <- data
+  dat <- as.matrix( df$k2/df$n )
+  
+  N <- sum( df$k2)/sum( df$n)
+  #f <- par$Var2
+  model <- simulate_M2( data)
+  
+  model <- as.data.frame(model)
+  model$type <- 'model'
+  colnames(model)[1] <- 'pmin'
+  colnames(dat)[1] <- 'pmin'
+  
+  dat <- as.data.frame(dat)
+  dat$type <- 'data'
+  data <- dat
+  
+  #and combine into your new data frame vegLengths
+  pmin1 <- rbind(data , model )
+  
+  #now make your lovely plot
+  if (logy == TRUE){
+    ggplot(pmin1, aes(pmin, colour = type)) + geom_density(alpha = 0.2 ) + coord_cartesian(xlim=xlim) +
+      scale_y_log10() + annotation_logticks() + theme(legend.position= "none",
+                                                      axis.text.y=element_blank() , axis.text.x=element_blank(),
+                                                      axis.line=element_blank() , axis.title.x=element_blank() , 
+                                                      axis.title.y=element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=1))} else
+                                                      {ggplot(pmin1, aes(pmin, fill = type)) + geom_density(alpha = 0.2 ) + coord_cartesian(xlim=xlim)}
+  return( pmin1)
+  
+  
+}
+
+M3 <- function( data , par , xlim = c(0,0.5) , logy =TRUE){
+  #data <- knPairs
+  #set.seed(71)
+  
+  df <- data
+  dat <- as.matrix( df$k2/df$n )
+  N <- par$Var1
+  f <- par$Var2
+  model <- simulate_fluc_tot( N , f , data )
+  
+  model <- as.data.frame(model)
+  model$type <- 'model'
+  colnames(model)[1] <- 'pmin'
+  colnames(dat)[1] <- 'pmin'
+  
+  dat <- as.data.frame(dat)
+  dat$type <- 'data'
+  data <- dat
+  
+  #and combine into your new data frame vegLengths
+  pmin1 <- rbind(data , model )
+  
+  #now make your lovely plot
+  if (logy == TRUE){
+    ggplot(pmin1, aes(pmin, colour = type)) + geom_density(alpha = 0.2 ) + coord_cartesian(xlim=xlim) +
+      scale_y_log10() + annotation_logticks() + theme(legend.position= c( 0.85 , 0.85),
+                                                      axis.text.y=element_blank() , axis.text.x=element_blank(),
+                                                      axis.line=element_blank() , axis.title.x=element_blank() , 
+                                                      axis.title.y=element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=1))} else
+                                                      {ggplot(pmin1, aes(pmin, fill = type)) + geom_density(alpha = 0.2 ) + coord_cartesian(xlim=xlim)}
+  
+  return( pmin1)
+}
+
+add_metrics <- function( mdf , i , model){
+  #i <- 5
+  fn <- paste("../data/" , filenames[i] , ".RData" , sep = "")
+  load( fn )
+  
+  name <- c(filenames[i] , exper[i])
+  alpha <- subset(alphas , species ==exper[i])$alpha
+  col <- c( 'Var1' , 'Var2' )
+  par <- as.data.frame( cbind(alpha, 0 ))
+  colnames( par ) <- col
+  
+  dm <- model( knPairs , par , xlim = c(0 , 0.5), logy = TRUE)
+  
+  d <- subset(dm, type == 'data')
+  m <- subset(dm, type == 'model')
+  
+  #stat_density(as.matrix(m))
+  
+  dens <- density(d$pmin ) #density of data and model
+  #plot( dens )
+  
+  dhist <- hist(d$pmin) #data hist
+  mhist <- hist(m$pmin) #model hist
+  
+  
+  dif <- sum((dhist$density - mhist$density)^2)/length(dhist$density)
+  mdf <- rbind( mdf , c( name[1] , "dif" , dif ))
+  
+  diflog <- sum(log(dhist$density/mhist$density)^2)/length(dhist$density)
+  mdf <- rbind( mdf , c( name[1] , "diflog" , diflog ))
+  
+  kl <- sum(dhist$density*log(dhist$density/mhist$density))
+  mdf <- rbind( mdf , c( name[1] , "kl" , kl ))
+  
+  return( mdf )
+  
+}
---
+++ model/scripts/pdf_metric.R
@@ -0,0 +1,101 @@
+###nice code
+
+#################################################
+###set wd, define libraries
+#################################################
+rm(list=ls(all=TRUE)) 
+setwd("~/repos//alt-splice-quantification/model//scripts")
+#setwd("~/splice/complete_cluster/")
+require(plyr)
+require(ggplot2)
+library(foreach)
+library(doMC)
+library( dplyr )
+#################################################
+###set sources, set.seed
+#################################################
+source("../modules/simLL_tot.R")
+source("../modules/simul_tot.R")
+source("../modules/plotting.R")
+source("../modules/analysis_functions.R")
+
+
+# file = ("../data_grids/zebrafish_grid1.RData")
+# par <- heat_analysis(file, TRUE)
+#par
+
+alphas <- read.csv( "alpha_mean_maxLL.csv")
+
+exper <- c( 'ar' , 'ce' , 'ch' , 'dr' , 'kc' , 'kp', 'lc' , 'lm' , 'lmb' , 'lmo' , 'lp' ,
+            'ls' , 'ly' , 'm8' , 'mb' , 'mc' , 'rb' , 'zf', 'gallus' , 'human' ,
+            'lizard' , 'xenopus' , 'pombe' )
+
+filenames <- c('arabidopsis' , 'celegans' , 'chlamy_2' , 'drosophila' , 'k562_chromatin',
+               'k562pap' , 'lymphoblast_cambodian' , 'lymphoblast_maya' ,
+               'lymphoblast_mbuti' , 'lymphoblast_mozabite' , 'lymphoblast_pathan' ,
+               'lymphoblast_san' , 'lymphoblast_yakut' , 'mouse_8_weeks' ,
+               'mouse_brain' , 'mouse_CNS' , 'rat_brain' , 'zebrafish' , 'gallus' ,
+               'human_brain' , 'lizard' , 'xenopus' , 'pombe')
+
+
+
+###initialize data frame of interest
+###
+mdf <- data.frame(transcriptome = NA , metric = NA , value = NA)
+#colnames( mdf ) <- c("transcriptome", "value")
+i <- 5
+add_metrics( mdf , i , M3)
+
+
+#####
+# Start the clock!
+ptm <- proc.time()
+
+for (i in 1:23){
+  for (m in c( M2 , M3 )){
+    mdf <- add_metrics( mdf , i , m)
+    print( i )
+  }
+}
+i
+
+add_metrics( mdf , i , M3)
+mdf1 <- mdf
+
+for (i in 1:23){
+  for (j in 1:20){
+    mdf <- add_metrics( mdf , i)
+    print( c(i,j) )
+    }
+}
+
+# Stop the clock
+proc.time() - ptm
+
+mdf <- add_metrics( mdf , 5)
+
+
+save(mdf1 , file = "measures1.RData")
+
+### stats by transcriptome and measure
+
+#mean( log(dhist$density/mhist$density)^2 )
+
+testdf <- group_by(mdf , transcriptome , metric)
+
+aa <- summarise(testdf , mean(v) , sd(v))
+aa$se <- aa$sd/sqrt(20)
+
+m <- ggplot( aa , aes( x = "transcriptome" , y = 'mean(v)' ) )
+
+m + geom_point()
+
+mdf$v <- as.numeric(mdf$value)
+
+metr = "diflog"
+df1 <- subset( aa , metric == metr )
+df1$m <- df1$`mean(v)`
+m <- ggplot( df1 , aes( x = transcriptome , y = m ) )
+limits <- aes(ymax = m + se, ymin = m-se )
+m + geom_point() + geom_errorbar( limits ) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
